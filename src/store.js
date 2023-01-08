493b281d7d3af64bb61ad26f626ebdd4432b793e
Rishi <rishi.singh@dianapps.com>
2023-01-08 12:08:11 +0530

    first commit

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './index'
 
 const initialState = {};
 const middleware = [thunk]
 
-const store = createStore(routeReducer, initialState, applyMiddleware(...middleware))
+const store = createStore(rootReducer, initialState, applyMiddleware(...middleware))
 
 export default store;
